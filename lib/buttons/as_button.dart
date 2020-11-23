ffd0c44e369a4dffa331d21d3f087252277ba968
laiiihz <laiiihz@outlook.com>
2020-11-23 15:16:44 +0800

    添加按钮

---
+++ lib/buttons/as_button.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+///安速按钮
+class ASButton extends StatefulWidget {
+  ASButton({Key key}) : super(key: key);
+
+  @override
+  _ASButtonState createState() => _ASButtonState();
+}
+
+class _ASButtonState extends State<ASButton> {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox();
+  }
+}
