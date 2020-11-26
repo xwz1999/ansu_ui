1226c69e85ecc1c1fa8c63577fb60516d2b0737e
laiiihz <laiiihz@outlook.com>
2020-11-26 08:46:06 +0800

    update as_divider

--- lib/ansu_ui.dart
+++ lib/ansu_ui.dart
@@ -4,13 +4,19 @@ export 'buttons/as_button.dart';
 export 'buttons/as_longbutton.dart';
 export 'buttons/as_back_button.dart';
 export 'buttons/as_numeric_button.dart';
+export 'buttons/as_bottom_button.dart';
+export 'buttons/as_gradientbutton.dart';
+
 export 'scaffold/as_scaffold.dart';
+
 export 'styles/as_colors.dart';
 export 'bar/as_tabbar.dart';
 export 'drawer/as_drawer.dart';
 export 'pickers/as_date_picker.dart';
 export 'pickers/as_picker_box.dart';
 export 'dialog/as_dialog.dart';
+export 'divider/as_divider.dart';
+export 'tag/as_tag.dart';
 
 export 'utils/screen_adapter.dart';
 
--- lib/divider/as_divider.dart
+++ lib/divider/as_divider.dart
@@ -1,12 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+///安速水平分割线
 class ASDivider extends StatelessWidget {
   final Color color;
   final double height;
   final double indent;
   final double endIndent;
   ASDivider({Key key, this.indent, this.endIndent})
+      : color = const Color(0xFFE9E9E9),
+        height = 1.w,
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Divider(
+      color: color,
+      height: height,
+      thickness: height,
+      indent: indent,
+      endIndent: endIndent,
+    );
+  }
+}
+
+///安速 垂直分割线
+class ASVDivider extends StatelessWidget {
+  final Color color;
+  final double height;
+  final double indent;
+  final double endIndent;
+  ASVDivider({Key key, this.indent, this.endIndent})
       : color = const Color(0xFFE9E9E9),
         height = 1.w,
         super(key: key);
