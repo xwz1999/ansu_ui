f6d02a1b95953038b3c44f141ec96b8aa3cb0398
zhang <494089941@qq.com>
2020-12-07 15:18:54 +0800

    update separate

--- lib/extension/separate_extension.dart
+++ lib/extension/separate_extension.dart
@@ -1,7 +1,7 @@
 import 'package:ansu_ui/ansu_ui.dart';
 import 'package:flutter/material.dart';
-extension SeparateExt on List {
-  List<Widget> sep([Widget separate]) {
+extension SeparateExt<T> on List {
+  List<T> sep({T separate}) {
     return List.generate(this.length * 2 - 1, (index) {
       if (index.isEven)
         return this[index ~/ 2];
