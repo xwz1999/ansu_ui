074b48181eb317b066b58ed55da3eba3eeff81de
laiiihz <laiiihz@outlook.com>
2020-12-17 17:06:48 +0800

    fix text extension issue

--- lib/extension/text_extension.dart
+++ lib/extension/text_extension.dart
@@ -1,25 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'text_style_extension.dart';
 
 extension TextExtension on Text {
   Text get bold {
     return Text(
       this.data,
-      style: this.style.copyWith(fontWeight: FontWeight.bold),
+      style: this.style?.bold ?? TextStyle().bold,
     );
   }
 
   Text size(double size) {
     return Text(
       this.data,
-      style: this.style.copyWith(fontSize: size.w),
+      style: this.style?.size(size) ?? TextStyle().size(size),
     );
   }
 
   Text color(Color color) {
     return Text(
       this.data,
-      style: this.style.copyWith(color: color),
+      style: this.style?.colorX(color) ?? TextStyle().colorX(color),
     );
   }
 }
--- lib/extension/text_style_extension.dart
+++ lib/extension/text_style_extension.dart
@@ -4,5 +4,5 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 extension TextStyleExtension on TextStyle {
   TextStyle get bold => this.copyWith(fontWeight: FontWeight.bold);
   TextStyle size(double size) => this.copyWith(fontSize: size.w);
-  TextStyle color(Color color) => this.copyWith(color: color);
+  TextStyle colorX(Color color) => this.copyWith(color: color);
 }
