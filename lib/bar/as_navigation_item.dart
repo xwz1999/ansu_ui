3b7e64475d76f548c776e09248dff83d22cb3c91
laiiihz <laiiihz@outlook.com>
2020-12-24 17:14:49 +0800

    as navigation item add tag

--- lib/bar/as_navigation_item.dart
+++ lib/bar/as_navigation_item.dart
@@ -1,19 +1,26 @@
+import 'package:ansu_ui/badge/as_badge.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 BottomNavigationBarItem asNavigationItem(
-    String path1, String path2, String title) {
+  String path1,
+  String path2,
+  String title, {
+  String tag,
+}) {
+  Widget icon = Image.asset(
+    path1,
+    height: 24.w,
+    width: 24.w,
+  );
+  Widget activeIcon = Image.asset(
+    path2,
+    height: 24.w,
+    width: 24.w,
+  );
   return BottomNavigationBarItem(
-    icon: Image.asset(
-      path1,
-      height: 24.w,
-      width: 24.w,
-    ),
-    activeIcon: Image.asset(
-      path2,
-      height: 24.w,
-      width: 24.w,
-    ),
+    icon: ASBadge(child: icon, tag: tag),
+    activeIcon: ASBadge(child: activeIcon, tag: tag),
     label: title,
   );
 }
