871f34fca3ab708947b8d85967e48c1d5eda25e3
laiiihz <laiiihz@outlook.com>
2021-03-01 13:30:51 +0800

    fix sepWidget extension when list empty

--- lib/extension/list_extension.dart
+++ lib/extension/list_extension.dart
@@ -3,6 +3,8 @@ import 'package:ansu_ui/extension/num_extension.dart';
 
 extension SeparateExt on List<Widget> {
   List<Widget> sepWidget({Widget separate}) {
+    if (this == null) return null;
+    if (this.isEmpty) return [];
     return List.generate(this.length * 2 - 1, (index) {
       if (index.isEven)
         return this[index ~/ 2];
