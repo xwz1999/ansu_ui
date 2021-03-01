dba1bd37471f4c7c2e738604868b3eb3b339d043
laiiihz <laiiihz@outlook.com>
2021-03-01 14:18:50 +0800

    refactor example extension view

--- example/lib/extension/example_num_ext.dart
+++ example/lib/extension/example_num_ext.dart
@@ -1,9 +1,8 @@
 import 'package:ansu_ui/ansu_ui.dart';
 import 'package:example/codeviewer/code_segments.dart';
-import 'package:example/common/code_view.dart';
+import 'package:example/component/example_scaffold.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:get/get.dart';
 
 class ExampleNumExt extends StatefulWidget {
   ExampleNumExt({Key key}) : super(key: key);
@@ -15,111 +14,97 @@ class ExampleNumExt extends StatefulWidget {
 class _ExampleNumExtState extends State<ExampleNumExt> {
   @override
   Widget build(BuildContext context) {
-    return ASScaffold(
+    return ExampleScaffold(
       title: 'Num Ext',
-      actions: [
-        IconButton(
-          icon: Icon(
-            Icons.code,
-            color: Colors.black54,
+      text: (context) => CodeSegments.numExt(context),
+      children: [
+        // BEGIN numExt
+        //Comma
+        ListTile(title: Text('Comma')),
+        ListTile(
+          leading: Chip(label: Text('.comma')),
+          title: Text('1000'),
+          subtitle: Text(1000.comma()),
+        ),
+        ListTile(
+          leading: Chip(label: Text('.comma')),
+          title: Text('1000000'),
+          subtitle: Text(1000000.comma()),
+        ),
+        ListTile(
+          leading: Chip(label: Text('.comma')),
+          title: Text('100.00'),
+          subtitle: Text(100.00.comma()),
+        ),
+        ListTile(
+          leading: Chip(label: Text('.comma')),
+          title: Text('0.123456'),
+          subtitle: Text(0.123456.comma()),
+        ),
+        ListTile(
+          leading: Chip(label: Text('.comma')),
+          title: Text('123456.789'),
+          subtitle: Text(123456.789.comma()),
+        ),
+        ListTile(
+          leading: Chip(label: Text('.comma')),
+          title: Text('123456.7899999'),
+          subtitle: Text(123456.7899999.comma(fixed: -1)),
+          trailing: Chip(label: Text('without Fixed')),
+        ),
+        //SizedBox
+        ListTile(title: Text('SizedBox')),
+        ListTile(
+          leading: Chip(label: Text('.wb')),
+          title: Text('Width Box'),
+          subtitle: Text('40.wb'),
+          trailing: Container(
+            height: 2.w,
+            width: 40.w,
+            color: Colors.red,
           ),
-          onPressed: () => Get.to(
-            CodeView(
-              text: (context) => CodeSegments.numExt(context),
+        ),
+        ListTile(
+          leading: Chip(label: Text('.hb')),
+          title: Text('Height Box'),
+          subtitle: Text('40.hb'),
+          trailing: Container(
+            height: 40.w,
+            width: 2.w,
+            color: Colors.red,
+          ),
+        ),
+        //Radius
+        ListTile(title: Text('Radius')),
+        ListTile(
+          leading: Chip(label: Text('.radius')),
+          title: Text('Radius Gen'),
+          subtitle: Text('10.radius'),
+          trailing: Container(
+            height: 30.w,
+            width: 30.w,
+            decoration: BoxDecoration(
+              borderRadius: 10.radius,
+              color: Colors.pink,
             ),
           ),
         ),
+        //EdgeInsets
+        ListTile(title: Text('EdgeInsets')),
+        ListTile(
+          leading: Chip(label: Text('.edge')),
+          title: Text('EdgeInsets Gen'),
+          subtitle: Text('10.edge'),
+          trailing: Container(
+            height: 30.w,
+            width: 30.w,
+            color: Colors.red,
+            padding: 10.edge,
+            child: Container(color: Colors.green),
+          ),
+        ),
+        // END
       ],
-      body: ListView(
-        children: [
-          // BEGIN numExt
-          //Comma
-          ListTile(title: Text('Comma')),
-          ListTile(
-            leading: Chip(label: Text('.comma')),
-            title: Text('1000'),
-            subtitle: Text(1000.comma()),
-          ),
-          ListTile(
-            leading: Chip(label: Text('.comma')),
-            title: Text('1000000'),
-            subtitle: Text(1000000.comma()),
-          ),
-          ListTile(
-            leading: Chip(label: Text('.comma')),
-            title: Text('100.00'),
-            subtitle: Text(100.00.comma()),
-          ),
-          ListTile(
-            leading: Chip(label: Text('.comma')),
-            title: Text('0.123456'),
-            subtitle: Text(0.123456.comma()),
-          ),
-          ListTile(
-            leading: Chip(label: Text('.comma')),
-            title: Text('123456.789'),
-            subtitle: Text(123456.789.comma()),
-          ),
-          ListTile(
-            leading: Chip(label: Text('.comma')),
-            title: Text('123456.7899999'),
-            subtitle: Text(123456.7899999.comma(fixed: -1)),
-            trailing: Chip(label: Text('without Fixed')),
-          ),
-          //SizedBox
-          ListTile(title: Text('SizedBox')),
-          ListTile(
-            leading: Chip(label: Text('.wb')),
-            title: Text('Width Box'),
-            subtitle: Text('40.wb'),
-            trailing: Container(
-              height: 2.w,
-              width: 40.w,
-              color: Colors.red,
-            ),
-          ),
-          ListTile(
-            leading: Chip(label: Text('.hb')),
-            title: Text('Height Box'),
-            subtitle: Text('40.hb'),
-            trailing: Container(
-              height: 40.w,
-              width: 2.w,
-              color: Colors.red,
-            ),
-          ),
-          //Radius
-          ListTile(title: Text('Radius')),
-          ListTile(
-            leading: Chip(label: Text('.radius')),
-            title: Text('Radius Gen'),
-            subtitle: Text('10.radius'),
-            trailing: Container(
-              height: 30.w,
-              width: 30.w,
-              decoration: BoxDecoration(
-                borderRadius: 10.radius,
-                color: Colors.pink,
-              ),
-            ),
-          ),
-          //EdgeInsets
-          ListTile(title: Text('EdgeInsets')),
-          ListTile(
-            leading: Chip(label: Text('.edge')),
-            title: Text('EdgeInsets Gen'),
-            subtitle: Text('10.edge'),
-            trailing: Container(
-              height: 30.w,
-              width: 30.w,
-              color: Colors.red,
-              padding: 10.edge,
-              child: Container(color: Colors.green),
-            ),
-          ),
-          // END
-        ],
-      ),
     );
   }
 }
--- example/lib/extension/example_string_ext.dart
+++ example/lib/extension/example_string_ext.dart
@@ -1,6 +1,7 @@
 import 'package:ansu_ui/ansu_ui.dart';
 import 'package:example/codeviewer/code_segments.dart';
 import 'package:example/common/code_view.dart';
+import 'package:example/component/example_scaffold.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -14,37 +15,23 @@ class ExampleStringExt extends StatefulWidget {
 class _ExampleStringExtState extends State<ExampleStringExt> {
   @override
   Widget build(BuildContext context) {
-    return ASScaffold(
+    return ExampleScaffold(
       title: 'String Ext',
-      actions: [
-        IconButton(
-          icon: Icon(
-            Icons.code,
-            color: Colors.black54,
-          ),
-          onPressed: () => Get.to(
-            CodeView(
-              text: (context) => CodeSegments.stringExt(context),
-            ),
-          ),
+      text: (context) => CodeSegments.stringExt(context),
+      children: [
+        // BEGIN stringExt
+        ListTile(
+          leading: Chip(label: Text('.phone')),
+          title: Text('18888888888'),
+          subtitle: Text('18888888888'.phone),
         ),
+        ListTile(
+          leading: Chip(label: Text('.securePhone')),
+          title: Text('18888888888'),
+          subtitle: Text('18888888888'.securePhone),
+        ),
+        // END
       ],
-      body: ListView(
-        children: [
-          // BEGIN stringExt
-          ListTile(
-            leading: Chip(label: Text('.phone')),
-            title: Text('18888888888'),
-            subtitle: Text('18888888888'.phone),
-          ),
-          ListTile(
-            leading: Chip(label: Text('.securePhone')),
-            title: Text('18888888888'),
-            subtitle: Text('18888888888'.securePhone),
-          ),
-          // END
-        ],
-      ),
     );
   }
 }
